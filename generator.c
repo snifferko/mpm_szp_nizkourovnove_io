e51fac5757b677e9ada4998b7667d215bfd31dad
Martin Kocun <mkocun@gmail.com>
2017-05-01 16:10:57 +0200

    pridany typ seq_t - linked list na ukladanie postupnosti, funkcie construktor, destruktor, printi,get_next

--- generator.c
+++ generator.c
@@ -13,6 +13,54 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct node{
+	struct node *next;
+	float *seq;
+	unsigned int len;
+} seq_t;
+
+seq_t *create_seq_t(float *seq, unsigned int len)
+{
+	seq_t *s=malloc(sizeof(seq_t));
+	s->seq=seq;
+	s->len=len;
+	s->next=NULL;
+	return s;
+}
+
+seq_t *get_next(seq_t *s)
+{
+	return s->next;
+}
+
+void free_seq_t(seq_t *s)
+{
+	if(s==NULL)
+		return;
+	else {
+		free_seq_t(get_next(s));
+		free(s->seq);
+		free(s);
+	}
+}
+
+void print_seq_t(seq_t *s){
+	int i;
+
+	if(s==NULL)
+		return;
+	else {
+		printf("%d: ",s->len);
+		
+		for(i=0;i<s->len;i++)
+			printf("%.1f ",s->seq[i]);
+		
+		printf("\n");
+		print_seq_t(get_next(s));
+	}
+}
 
 int main(int argc, char **argv){
 	int file;
@@ -34,6 +82,17 @@ int main(int argc, char **argv){
 		printf("Subor sa nepodarilo otvorit.\n");
 		return CHYBA;
 	} 
+
+
+	//TEST
+
+	float A[]={3.5,1.8,-2.4,5.665,5};
+	float *a=malloc(sizeof(A));
+	memcpy(a,A,sizeof(A));
+	seq_t *s=create_seq_t(a,5);
+	print_seq_t(s);
+	free_seq_t(s);
+	
 
 	close(file);
 }
